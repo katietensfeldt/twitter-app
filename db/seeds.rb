93a84f6678adc901113409b7b92b69e0efd66665
Katie Tensfeldt <kmtensfeldt@gmail.com>
2021-05-19 15:39:06 -0700

    Adds data to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!([
+  {username: "happytweeter123", email: "happytweeter@gmail.com"},
+  {username: "randomstuff789", email: "randomstuff789@gmail.com"},
+  {username: "bubbles", email: "bubbles@gmail.com"}
+])
+
+Tweet.create!([
+  {text: "This is my first tweet! I can't wait to say more things", user_id: 1},
+  {text: "My new phone is a fairytale, and I want to start a blog. Fabulous insane nail clippings, man.", user_id: 3},
+  {text: "My mom is very timely, and I want to get more done. A bit of creepy ambition, man. #choppedfest #randomtweet", user_id: 2},
+  {text: "My script is not for you, and I want to get more email. Fabulous great caffeine, forever. #maltedgold #randomtweet", user_id: 1},
+  {text: "My very soul is a disaster, and I want to be famous. Let there be stellar heroes, IMHO. #sillypop #randomtweet", user_id: 2}
+])
+
